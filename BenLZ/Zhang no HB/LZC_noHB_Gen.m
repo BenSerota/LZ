deacea8996f45a5698e5d92ac58717ccbf1a8e7d
BenSerota <serota@post.bgu.ac.il>
2018-02-27 11:06:19 +0200

    important - first working draft

--- BenLZ/Zhang no HB/LZC_noHB_Gen.m
+++ BenLZ/Zhang no HB/LZC_noHB_Gen.m
@@ -2,8 +2,8 @@
 
 clear
 clc
-% start_ben
-global out_paths conds subconds num lim plothb E_T_flag %#ok<NUSED>
+start_ben
+global out_paths conds subconds num lim plothb E_T_flag  %#ok<NUSED>
 DOC_basic
 LZC_noHB_param
 
