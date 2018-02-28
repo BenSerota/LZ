6766fff562ac2a2af102eebfabd2b0752a8dc6c3
BenSerota <serota@post.bgu.ac.il>
2018-03-01 00:58:03 +0200

    important

--- BenLZ/Zhang no HB/LZC_noHB_Gen.m
+++ BenLZ/Zhang no HB/LZC_noHB_Gen.m
@@ -9,19 +9,38 @@ LZC_noHB_param
 
 [LZCs_per_cond] = LZC_noHB(data_ratio, rates, rate_flag, event_flag);
 
-save('2ndLZCnohb');
+% save WS
+SaveUniqueName('LZC_nohb', LZC_nohb_outpath)
 
 %% plotting
 
-
-
  if ~task_flag
      LZCs_per_cond = cellfun(@(x) mean(x,2),LZCs_per_cond,'UniformOutpu',false');
+ else
+     error('task_flag is 1, but I have not set up a plotting scheme for this yet');
  end
-%  
-%  LZCs_per_cond = cell2mat(LZCs_per_cond);
-%  plot(1:length(conds),LZCs_per_cond)
-%  num2cell(LZCs_per_cond)
+
+ 
  figitup(LZCs_per_cond,'LZC per condition',0)
  
+%% Excessory Funcs
+ 
+function [] = SaveUniqueName(root_name,location)
+if ~isstring(root_name) && ~ischar(root_name)
+    error('1st input (file name) must be of class char or string')
+end
+
+if ~isstring(location) && ~ischar(location)
+    error('2nd input (file directory) must be of class char or string')
+end
+
+cl = fix(clock);
+cl(end) = [];
+stamp = strrep(mat2str(cl),' ','_');
+stamp = strrep(stamp,'[','');
+stamp = strrep(stamp,']','');
+UniqueName = [root_name '_' stamp];
+cd (location)
+save (UniqueName)
+end
  
--- BenLZ/Zhang no HB/LZC_noHB_param.m
+++ BenLZ/Zhang no HB/LZC_noHB_param.m
@@ -1,16 +1,16 @@
 % LZC_noHB_param
 %(data_ratio, rates, rate_flag, event_flag ,task_flag);
-global task_flag
+global task_flag plot_hb
 
 %% direct inputs
 % how much of data to take into consideration (~(0,1))?
-data_ratio = 0.2; % (rate min = 0.042)
+data_ratio = 0.3; % (rate min = 0.042)
 
 %determine event rates
 rates = [5 10 20 30 40];
 
 %use 5 different event rates(=1) or only one (first) (=0)?
-rate_flag = 0;
+rate_flag = 3;
 
 %use zscore only(=0) or hilbert transform on zscore(=1)?
 event_flag = 1; 
@@ -21,14 +21,14 @@ task_flag = 0;
 
 %% globals
 % r chooses how many of the first ICs to reject in each dataset:
-num = 1;
+num = 30;
 
 % what's the component beyond which we don't care if there is HB (as
 % components are organized hierarchically).
-lim = inf;
+lim = 30;
 
 % calc LZC by row (temporal; 0) or column (sptial; 1)
 E_T_flag = 0;
 
 % % plot HB EEG ?
-% plothb = 0;
+plot_hb = 0;
